f7b346cf36e62861cc803abbc432270cf629ba3c
zMariner <138823291+zMariner@users.noreply.github.com>
2023-10-26 00:10:50 +0300

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,22 +1,11 @@
 window.addEventListener('DOMContentLoaded', function() {
   var levels = [
-      { name: '#9 - Firework', id: '75206202', author: 'TrickGMD', Beaten: '20 Oct 2023', videoUrl: 'https://youtu.be/YZAoOEVvK8I', type: 'Level' },
-    { name: '#81 - Frozen Cave', id: '68908396', author: 'TrueParadoxTeam', Beaten: '18 Apr 2023', videoUrl: 'https://www.youtube.com/watch?v=J57GlLpUbRg', type: 'Level' },
-    { name: '#90 - Wasureta', id: '64658786', author: 'HelpegasuS', Beaten: '14 Mar 2023', videoUrl: 'https://www.youtube.com/watch?v=K5FK2kOptzI', type: 'Level' },
-    { name: '#248 - Dolos', id: '58417850', author: 'Enzeux', Beaten: '1 Apr 2023', videoUrl: 'https://www.youtube.com/watch?v=iscKMHv-Lsk', type: 'Level' },
-    { name: '#N/A - Moment', id: '68848817', author: 'lexycat', Beaten: '22 Oct 2023', videoUrl: 'https://www.youtube.com/watch?v=O6N_7RZsogc', type: 'Level' },
-    { name: '#N/A - Shitty Minusdry', id: '90383862', author: 'Wiktor and Mystix', Beaten: '29 Apr 2023', videoUrl: 'https://www.youtube.com/watch?v=yd3RoTdunJo', type: 'Level' },
-    { name: '#261 - Bloodbath', id: '10565740', author: 'Riot', Beaten: '23 Aug 2022', videoUrl: 'https://www.youtube.com/watch?v=W8cPS8VTZp0', type: 'Level' },
-    { name: '#325 - Prismatic Haze', id: '59899374', author: 'Cirtrax', Beaten: '30 Mar 2023', videoUrl: 'https://www.youtube.com/watch?v=McXMhw1kafg', type: 'Level' },
-    { name: '#347 - Cataclysm', id: '3979721', author: 'Ggb0y', Beaten: '2 Aug 2022', videoUrl: 'https://www.youtube.com/watch?v=0r-7XShVfeg', type: 'Level' },
-    { name: '#N/A - Acu', id: '61079355', author: 'Neigefeu', Beaten: '27 Dec 2022', videoUrl: 'https://www.youtube.com/watch?v=uraoLJbRsfk', type: 'Level' },
-    { name: '#N/A - Hidden in the Sand', id: '87481937', author: 'ASBCHazel', Beaten: '3 Mar 2023', videoUrl: 'https://www.youtube.com/watch?v=b6Fr7SEcy84', type: 'Level' },
-    { name: '#N/A - Spin Right Round', id: '91737656', author: 'Blueskii', Beaten: '8 Aug 2023', videoUrl: 'https://youtu.be/lc2inLYqBb4', type: 'Level' },
-    { name: '#N/A - Firewall', id: '13437081', author: 'Hinds', Beaten: '28 Jul 2022', videoUrl: 'https://www.youtube.com/watch?v=A2-I-os-oKs', type: 'Level' },
-    { name: '#N/A - Supersonic', id: '4706930', author: 'ZenticAlpha', Beaten: '18 Jul 2022', videoUrl: 'https://youtu.be/l97leHRa4xU?si=6RJFxsNHgBzn80xb', type: 'Level' },
-    { name: '#N/A - My Time', id: '82595578', author: 'Blueskii', Beaten: '11 Nov 2022', videoUrl: 'https://youtu.be/b0VpGcJkhHo', type: 'Level' },
-    { name: '#N/A - 8o', id: '9145301', author: 'Zobros', Beaten: '8 Jul 2022', videoUrl: 'https://www.youtube.com/watch?v=Q-vbTISgQBs', type: 'Level' },
-    { name: '#N/A - Windy Landscape', id: '4957691', author: 'WOOGI1411', Beaten: '6 Jul 2022', videoUrl: 'https://www.youtube.com/watch?v=8r-RiL_kc1w', type: 'Level' }
+    { name: 'Acheloos', id: '93991257', author: 'Mariner', Beaten: '31 Aug 2023', videoUrl: 'https://www.youtube.com/watch?v=v38Lpv_8Hmk', type: 'Challenge' },
+    { name: 'Banana Beater', id: '91149251', author: 'Smickalv', Beaten: '15 Aug 2023', videoUrl: 'https://www.youtube.com/watch?v=x1-Udnf1kVw', type: 'Challenge' },
+    { name: 'Under Heroine I', id: '92182309', author: 'Mariner', Beaten: '6 Jul 2023', videoUrl: 'https://www.youtube.com/watch?v=4cK_ZNj7D7c', type: 'Challenge' },
+    { name: 'The Xylz Trials', id: '90885542', author: 'baconmonke', Beaten: '31 Aug 2023', videoUrl: 'https://www.youtube.com/watch?v=EN5BWf4cMsI', type: 'Challenge' },
+    { name: 'Cave of Lilys', id: '91130171', author: 'WDWiktor', Beaten: '30 May 2023', videoUrl: 'https://youtu.be/6B8mPt69P4I', type: 'Challenge' },
+    { name: 'In the End', id: '87989862', author: 'maaaaaaaaaaark2', Beaten: '13 May 2023', videoUrl: 'https://youtu.be/c44KdklvQ1w', type: 'Challenge' }
   ];
 
   var levelList = document.getElementById('levelList');
@@ -51,22 +40,23 @@ window.addEventListener('DOMContentLoaded', function() {
     listItem.appendChild(listNumber);
     listItem.appendChild(listThumbnail);
     listItem.appendChild(listInfo);
-    levelList.appendChild(listItem);
+    challengeList.appendChild(listItem);
 
   
 // Display the popup
 listItem.addEventListener('click', function() {
-  // Create an img element for the thumbnail
-  var thumbnailImage = document.createElement('img');
+  // Get the video ID from the video URL
   var videoId = extractVideoId(levels[index].videoUrl);
-  var thumbnailUrl = 'https://img.youtube.com/vi/' + videoId + '/mqdefault.jpg';
-  thumbnailImage.src = thumbnailUrl;
 
-  // Create an anchor element for the link
-  var thumbnailLink = document.createElement('a');
-  thumbnailLink.href = levels[index].videoUrl;
-  thumbnailLink.target = '_blank';
-  thumbnailLink.appendChild(thumbnailImage);
+  // Create the iframe element for the embedded video
+  var iframe = document.createElement('iframe');
+  iframe.src = 'https://www.youtube.com/embed/' + videoId;
+  iframe.width = '560';
+  iframe.height = '315';
+  iframe.frameborder = '0';
+  iframe.allowfullscreen = true;
+  iframe.style.border = 'none';
+  iframe.style.borderRadius = "10px";
 
   // Get the popup element
   var popup = document.getElementById('popup');
@@ -75,8 +65,8 @@ listItem.addEventListener('click', function() {
   var popupVideo = document.getElementById('popup-video');
   popupVideo.innerHTML = '';
 
-  // Append the thumbnail to the popup video
-  popupVideo.appendChild(thumbnailLink);
+  // Append the iframe to the popup video
+  popupVideo.appendChild(iframe);
 
   // Populate the popup with the corresponding information
   document.getElementById('popup-name').textContent = levels[index].name;
@@ -92,6 +82,13 @@ listItem.addEventListener('click', function() {
 document.getElementById('popup-close').addEventListener('click', function() {
   var popup = document.getElementById('popup');
   popup.classList.remove('visible');
+
+  // Get the iframe element
+  var iframe = document.getElementById('popup-video').querySelector('iframe');
+
+  // Pause the video and reset the source
+  iframe.contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
+  iframe.src = '';
 });
 
   function extractVideoId(url) {
