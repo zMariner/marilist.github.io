3e4dfa81728fe2ca3c91125ff6c0d0780ec59283
zMariner <138823291+zMariner@users.noreply.github.com>
2025-05-01 21:41:43 +0300

    Added Combustion

--- scriptladder.js
+++ scriptladder.js
@@ -1,7 +1,7 @@
 window.addEventListener('DOMContentLoaded', function() {
   var levels = [
     { name: '#19 - Poocubed', id: '85133223', author: 'Liisp', Beaten: 'N/A', videoUrl: 'https://youtu.be/ms5xPzWFNYQ?si=Imxl9Zd43-27uHvG', type: 'Level' },
-    { name: '#29 - Combustion', id: '94359172', author: 'Cersia', Beaten: 'N/A', videoUrl: 'https://youtu.be/iOelI8rcedw', type: 'Level' },
+    { name: '#29 - Combustion', id: '94359172', author: 'Cersia', Beaten: 'N/A', videoUrl: 'https://youtu.be/7CN82T7zdvI', type: 'Level' },
     { name: '#??? - ???', id: 'N/A', author: 'N/A', Beaten: 'N/A', videoUrl: 'https://youtu.be/Ev0hhQRpWxk', type: 'Level' },
     { name: '#??? - ???', id: 'N/A', author: 'N/A', Beaten: 'N/A', videoUrl: 'https://youtu.be/I27TjqthWco', type: 'Level' },
     { name: '#??? - ???', id: 'N/A', author: 'N/A', Beaten: 'N/A', videoUrl: 'https://youtu.be/I27TjqthWco', type: 'Level' },
